e6c33f0015508228d951d97be1111de36c6d0456
Cristiano Amorim <cristianoretiro2003@gmail.com>
2023-03-27 15:51:35 -0300

    Created tickets section

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,11 @@
 import Home from './routes/Home.html';
-import RequestGet from './routes/1-request-get/RequestGet.html';
-import RequestPost from './routes/2-request-post/RequestPost.html';
-import Data from './routes/3-dados/Home.html';
-import DataList from './routes/3-dados/List.html';
-import DataAdd from './routes/3-dados/Add.html';
-import DataUser from './routes/3-dados/User.html';
-import Printing from './routes/4-impressao/Home.html';
+import Tickets from './routes/tickets/Tickets.html';
+import Ticket from './routes/tickets/Ticket.html';
+import Purchase from './routes/tickets/Purchase.html';
 
 export default {
   '/': Home,
-  '/1-request-get': RequestGet,
-  '/2-request-post': RequestPost,
-  '/3-dados': Data,
-  '/3-dados/add': DataAdd,
-  '/3-dados/list': DataList,
-  '/3-dados/user': DataUser,
-  '/4-impressao': Printing,
+  '/tickets': Tickets,
+  '/tickets/:id': Ticket,
+  '/tickets/:id/purchase': Purchase,
 };
